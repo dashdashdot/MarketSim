8aa6206c0d484e74c121baea8753215b1bd3db8c
dashdashdot <dashdashdot@users.noreply.github.com>
2014-11-12 20:53:23 +0000

    Encouraged more trading in the perf test.

--- exchange/src/test/java/org/gnw/mktsim/exchange/TestOrderBookPerf.java
+++ exchange/src/test/java/org/gnw/mktsim/exchange/TestOrderBookPerf.java
@@ -15,7 +15,7 @@ public class TestOrderBookPerf {
         for (int i = 0; i < 1000; i++) {
             boolean side = numGen.nextBoolean();
             long quantity = numGen.nextInt(300) + 1L;
-            double price = imnt.getLastPrice() + (side ? 1 : -1) * (numGen.nextInt(10) - 8.0) / 10.0;
+            double price = imnt.getLastPrice() + (side ? 0.1 : -0.1) * (numGen.nextInt(10) - 6);
             Order order = new Order("test1K", Integer.toString(i), imnt, side, quantity, price);
             book.addOrder(order);
         }
